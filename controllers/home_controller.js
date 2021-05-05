d4823fed22c2f4613d1381dfef522cb90060f62e
Deepak Awal <awaldeepak.1997@gmail.com>
2021-05-05 21:55:02 +0530

    Added a controller and a router for Home

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+module.exports.home = function(req, res){
+    return res.end('<h1>Codeial Home</h1>');
+}
+
+
+//module.exports.actionName = function(req, res){}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use('/', require('./routes'));
+
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in connecting to the server: ${err}`);
