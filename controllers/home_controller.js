c72c544fa273cc304619d629bc1acdb0e9231b6d
Deepak Awal <awaldeepak.1997@gmail.com>
2021-05-05 22:27:00 +0530

    Added a view for the home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Codeial Home</h1>');
+    return res.render('home', {
+        'title': 'Codeial'
+    });
 }
 
 
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 app.use('/', require('./routes'));
 
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in connecting to the server: ${err}`);
