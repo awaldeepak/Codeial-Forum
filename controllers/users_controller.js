f4c02c40136c577a2fdb4eaf639f1dd94072ae68
Deepak Awal <awaldeepak.1997@gmail.com>
2021-05-08 19:46:15 +0530

    Added Express Layouts for Home and User Profile Page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,5 @@
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
-        'title': 'Users'
+        'title': 'Profile Page'
     });
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(expressLayouts);
 app.use('/', require('./routes'));
 
 
