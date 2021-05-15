a76f3c65c8905c147b80f9a8c02f4a8a5a0a98ab
Deepak Awal <awaldeepak.1997@gmail.com>
2021-05-15 21:59:37 +0530

    Added: Destroy Post along with all its comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.createPost = function(req, res){
 
@@ -15,4 +16,29 @@ module.exports.createPost = function(req, res){
         return res.redirect('back');
     });
    
+}
+
+module.exports.destroyPost = function(req, res){
+
+    Post.findById(req.params.id, function(err, post){
+        if(err){
+            console.log('Error in finding the post');
+            return;
+        }
+
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if(err){
+                    console.log('Error in deleting the comments of this post');
+                    return;
+                }
+
+                return res.redirect('back');
+            });
+        } else {
+            return res.redirect('back');
+        }
+    });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,6 @@ const passport = require('passport');
 const postsController = require('../controllers/posts_controller');
 
 router.post('/create-post', passport.checkAuthentication, postsController.createPost);
-
+router.get('/destroy-post/:id', passport.checkAuthentication, postsController.destroyPost);
 
 module.exports = router;
