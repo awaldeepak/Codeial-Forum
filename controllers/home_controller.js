3cb5a65b631bb35fb86ae59ee3a5a95a68185b10
Deepak Awal <awaldeepak.1997@gmail.com>
2021-05-29 13:52:22 +0530

    Added: Async await for Home Controller Actions

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,32 +3,30 @@ const Comment = require('../models/comment');
 const User = require('../models/user');
 
 //An Action for the Codeial Home Page
-module.exports.home = function(req, res){
+module.exports.home = async function(req, res){
 
-    //console.log(req.cookies);   //Print the browser cookie on console
+    try{
+        //Populate the user of each post
 
-    // res.cookie('user_id', 25);  //Alter the browser cookie from constoller
-
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        if(err){ console.log('Error in fetching the posts'); }
-
-            User.find({}, function(err, users){
-
-                if(err){ console.log('Error in fetching the users'); }
-                
-                return res.render('home', {
-                    title: 'Codeial',
-                    posts: posts,
-                    all_users: users    
-            });
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
-    });
+    
+        let users = await User.find({});
+
+        return res.render('home', {
+        title: 'Codeial',
+        posts: posts,
+        all_users: users    
+        });
+
+    } catch(err) {
+        console.log("Error: ", err);
+        return;
+    }
 }
